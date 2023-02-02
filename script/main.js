a5ded25c02f43c8e6544a2632a4d60b0f9f431cc
HedaKS <118710428+HedaKS@users.noreply.github.com>
2023-02-02 12:23:34 +0400

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,9 @@
+
+
+
+  function showScript() {
+    var script = document.createElement('script');
+    script.src = "https://api.blablagues.net/blablagues.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
